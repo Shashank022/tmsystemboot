a821c333b8edc334d6325454809ef51a5fe2cfee
srivi <svc2301@gmail.com>
2020-10-20 16:38:05 -0500

    Updated changes in GroupRepository as application is failing to run

--- src/main/java/com/springboot/daoImpl/GroupRepositoryImpl.java
+++ src/main/java/com/springboot/daoImpl/GroupRepositoryImpl.java
@@ -2,14 +2,18 @@ package com.springboot.daoImpl;
 
 import java.util.List;
 
+import org.springframework.stereotype.Repository;
+
 import com.springboot.model.Event;
 import com.springboot.repository.GroupRepository;
 
+//@Repository
 public class GroupRepositoryImpl implements GroupRepository {
 
-	@Override
-	public List<Event> getListofJoinData() {
-		return null;
-	}
+//	@Override
+//	public List<Event> getListofJoinData() {
+//	
+//		return null;
+//	}
 
 }
--- src/main/java/com/springboot/repository/GroupRepository.java
+++ src/main/java/com/springboot/repository/GroupRepository.java
@@ -1,9 +1,13 @@
 package com.springboot.repository;
 
 import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
 import com.springboot.model.Event;
 
+
 public interface GroupRepository {
-	public List<Event> getListofJoinData();
+//	public List<Event> getListofJoinData();
 	
 }
