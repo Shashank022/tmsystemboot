8bf2a78085897853e312154892306f690e44082b
Shashank <shashank022@gmail.com>
2020-10-26 17:50:41 -0500

    updated changes

--- src/main/java/com/springboot/controller/TaskController.java
+++ src/main/java/com/springboot/controller/TaskController.java
@@ -1,17 +1,13 @@
 package com.springboot.controller;
 
 import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
-import com.springboot.model.Event;
 import com.springboot.model.Task;
-import com.springboot.service.EventService;
 import com.springboot.service.TaskService;
 
 public class TaskController {
